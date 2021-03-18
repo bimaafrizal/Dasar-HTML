d053693b048b818c2094e36f364160895c3461e4
Bima Afrizal Malna <72643530+bimaafrizal@users.noreply.github.com>
2021-03-19 01:15:38 +0700

    Add files via upload

---
+++ arraysNumeric.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $buah[0] = "mangga";
+    $buah[1] = "apel";
+    $buah[2] = "pisang";
+    $buah[3] = "jambu";
+    echo "Berikut ini adalah 2 buah yang saya suka, yaitu " .
+        $buah[0] . " & " . $buah[1];
+    echo "<br>Namun, saya juga suka buah " .
+        $buah[2] . " & " . $buah[3];
+    ?>
+</body>
+
+</html>
