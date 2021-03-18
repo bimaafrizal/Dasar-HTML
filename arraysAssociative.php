543e8cd3471a72d27ead7157e4e0c4255f041aa4
Bima Afrizal Malna <72643530+bimaafrizal@users.noreply.github.com>
2021-03-19 01:15:55 +0700

    Add files via upload

---
+++ arraysAssociative.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $harga["mangga"] = 200;
+    $harga["apel"] = 400;
+    $harga["pisang"] = 600;
+    $harga["jambu"] = 220;
+    echo "harga mangga: Rp" . $harga["mangga"] . "<br>";
+    echo "harga apel: Rp" . $harga["apel"] . "<br>";
+    echo "harga pisang: Rp" . $harga["pisang"] . "<br>";
+    echo "harga jambu: Rp" . $harga["jambu"];
+    ?>
+</body>
+
+</html>
